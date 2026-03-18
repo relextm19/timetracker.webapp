e18f8c44ead044292bfa6cd5638b35a3fac7f110
Mateusz Kielb <relextm19@gmail.com>
2026-03-18 18:57:24 +0100

    added an api keys route

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <header class="bg-black text-white shadow-md">
-    <div class="flex items-center justify-between h-16 px-8 w-full">
-      <div class="text-2xl font-bold">
-        TimeTracker
-      </div>
+    <header class="bg-black text-white shadow-md">
+        <div class="flex items-center justify-between h-16 px-8 w-full">
+            <div class="text-2xl font-bold">
+                TimeTracker
+            </div>
 
-      <nav class="flex gap-6">
-        <MenuElement to="/login" text="Login" />
-        <MenuElement to="/dashboard" text="Dashboard" />
-        <MenuElement to="/" text="Home" />
-      </nav>
-    </div>
-  </header>
+            <nav class="flex gap-6">
+                <MenuElement to="/api_keys" text="Api keys" />
+                <MenuElement to="/dashboard" text="Dashboard" />
+                <MenuElement to="/" text="Home" />
+            </nav>
+        </div>
+    </header>
 
-  <main class="bg-black text-white min-h-screen">
-    <router-view></router-view>
-  </main>
+    <main class="bg-black text-white min-h-screen">
+        <router-view></router-view>
+    </main>
 </template>
 
 <script setup lang="ts">
--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -9,6 +9,7 @@ const routes = [
     { path: '/register', component: Register, meta: { public: true } },
     { path: '/', component: Home },
     { path: '/dashboard', component: Dashboard },
+    { path: '/api_keys', component: Dashboard },
 ];
 
 const router = createRouter({
@@ -19,7 +20,6 @@ const router = createRouter({
 let loggedIn = false;// cache the value so i dont have to hit /api/checkAuth every request. This is not a security issue cause i still have an auth middleware on the backend so if someone plays arround with the client side the app still wont let them acces data
 
 router.beforeEach(async (to, _, next) => {
-    console.log(loggedIn)
     if (to.meta.public) {
         if (loggedIn) return next('/');
         return next();
@@ -31,7 +31,6 @@ router.beforeEach(async (to, _, next) => {
         console.log(res)
         if (res.ok) {
             loggedIn = true;
-            console.log(loggedIn)
             next();
         } else next('/login');
     } catch {
