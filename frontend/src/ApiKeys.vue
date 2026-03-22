07866c03b91f834940df3f1569642f73eeeac1d5
Mateusz Kielb <relextm19@gmail.com>
2026-03-22 18:34:48 +0100

    added a modal component

--- frontend/src/ApiKeys.vue
+++ frontend/src/ApiKeys.vue
@@ -1,7 +1,5 @@
 <template>
-    <div v-if="showNewKeyModal"
-        class="absolute top-1/2 left-1/2 -translate-1/2 flex justify-center items-center flex-col border-2 border-white rounded-lg gap-4 p-4 z-50 bg-black">
-        <h2 class="text-2xl tracking-wider">Add API key</h2>
+    <Modal v-if="showNewKeyModal" title="Add API key">
         <form @submit.prevent="addKey" class="flex justify-center items-center flex-col gap-4">
             <input v-model="newKeyName" type="text"
                 class="border-2 border-gray-400 rounded-xs focus:outline-0 focus:ring-1 focus:ring-white"
@@ -10,10 +8,8 @@
                 class="bg-transparent p-1 w-full text-white border border-white rounded-xs hover:bg-white hover:text-black transition duration-200 cursor-pointer focus:outline-0 focus:ring-1 focus:ring-white"
                 type="submit" value="Add">
         </form>
-    </div>
-    <div v-if="showKeyModal"
-        class="absolute top-1/2 left-1/2 -translate-1/2 flex justify-center items-center flex-col border-2 border-white rounded-lg gap-4 p-4 z-50 bg-black">
-        <h2 class="text-2xl tracking-wider">Your API key</h2>
+    </Modal>
+    <Modal v-if="showKeyModal" title="Your API key">
         <div class="flex justify-center items-center">
             <span>{{ newKeyValue }}</span>
             <button @click="copyToClipboard(newKeyValue)"
@@ -22,10 +18,14 @@
 
                 <span class="absolute inset-0 flex items-center justify-center transition-all duration-300 ease-in-out"
                     :class="copied ? 'opacity-0 scale-50 rotate-90' : 'opacity-100 scale-100 rotate-0'">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-5 h-5">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5-1.5H13.5a3.75 3.75 0 0 1 3.75 3.75v6.75a9.06 9.06 0 0 1-1.5 1.5Zm0 0c.342 0 .684.048 1.012.144.405.118.784.329 1.11.616l2.122 2.122c.287.287.498.666.616 1.071m-9.444-11.444L15 9.75M21 12v4.5A2.25 2.25 0 0 1 18.75 18.75h-4.5" />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-clipboard-x" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                            d="M6.146 7.146a.5.5 0 0 1 .708 0L8 8.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 9l1.147 1.146a.5.5 0 0 1-.708.708L8 9.707l-1.146 1.147a.5.5 0 0 1-.708-.708L7.293 9 6.146 7.854a.5.5 0 0 1 0-.708" />
+                        <path
+                            d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z" />
+                        <path
+                            d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z" />
                     </svg>
                 </span>
 
@@ -40,11 +40,11 @@
             </button>
         </div>
         <span class="text-red-600">This is your only chance to save the key!</span>
-        <button @click="showKeyModal = !showKeyModal"
+        <button @click="showKeyModal = !showKeyModal; copied = false;"
             class="bg-black p-1 w-full text-white border border-white rounded-xs hover:bg-white hover:text-black transition duration-200 cursor-pointer focus:outline-0 focus:ring-1 focus:ring-white">
             Close
         </button>
-    </div>
+    </Modal>
     <div :class="{ 'blur-xs': showNewKeyModal || showKeyModal }">
         <p class="text-3xl font-semibold uppercase tracking-wider mb-6 text-center text-white">
             API keys
@@ -96,6 +96,7 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import { formatToDate } from './utils/formatTime';
+import Modal from './components/Modal.vue';
 
 type APIKey = {
     id: number
---
+++ frontend/src/components/Modal.vue
@@ -0,0 +1,14 @@
+<template>
+    <div
+        class="absolute top-1/2 left-1/2 -translate-1/2 flex justify-center items-center flex-col border-2 border-white rounded-lg gap-4 p-4 z-50 bg-black">
+        <h2 class="text-2xl tracking-wider">{{ props.title }}</h2>
+        <slot>
+        </slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    title: string
+}>()
+</script>
