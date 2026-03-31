3d670e18f912972fe494fc333c722df00b43f89f
Mateusz Kielb <relextm19@gmail.com>
2026-03-31 10:25:01 +0200

    changes to the dropdown

--- frontend/src/components/DisplaySwitch.vue
+++ frontend/src/components/DisplaySwitch.vue
@@ -1,25 +1,34 @@
 <template>
-    <div class="flex justify-center items-center flex-col w-1/6 text-wrap relative">
-        <div class="flex justify-center items-center w-full">
-            <span class="flex-5/6 text-center">
+    <div class="flex flex-col w-48 justify-center items-center relative bg-black z-50">
+
+        <div class="flex items-center w-full cursor-pointer py-1" @click="dropped = !dropped">
+
+            <div class="w-6 opacity-0 pointer-events-none"></div>
+
+            <span class="flex-1 text-center whitespace-nowrap px-4">
                 {{ selected }}
             </span>
-            <div :class="dropped ? 'rotate-180' : 'rotate-0'" @click="dropped = !dropped;"
-                class="cursor-pointer transform transition-transform duration-200 absolute right-0">
+
+            <div class="w-6 flex justify-center items-center transform transition-transform duration-200"
+                :class="dropped ? 'rotate-180' : 'rotate-0'">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M15 12l-5-5-5 5" />
                 </svg>
             </div>
+
         </div>
-        <div :class="dropped ? 'hidden' : ''" class="w-full flex items-center justify-center flex-col self-start">
-            <div v-for="option of options" @click="updateSelected(option)" class="cursor-pointer">
+
+        <div :class="dropped ? 'hidden' : 'flex'"
+            class="absolute top-full left-0 w-full flex-col m-y-1 items-center bg-black truncate">
+            <div v-for="option of options" :key="option" @click="updateSelected(option)"
+                class="cursor-pointer w-full text-center  hover:bg-gray-900">
                 {{ option }}
             </div>
         </div>
+
     </div>
 </template>
-
 <script setup lang="ts">
 import { GroupBy } from '@/Dashboard.vue';
 import { ref } from 'vue';
